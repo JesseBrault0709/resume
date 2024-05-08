c24ba9ab57fbef642d5a18610d17c44300fb32ea
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2024-05-08 09:31:55 +0200

    Better cli and build directories.

--- src/build.rb
+++ src/build.rb
@@ -11,18 +11,39 @@ class BuildCli < Thor
   end
 
   desc 'build', 'build the résumé'
-  option :job_title, default: 'Full-Stack Software Engineer'
+  option :build_name, aliases: :n, default: 'general'
+  option :include_time, aliases: :t, type: :boolean
+  option :job_title, aliases: :j, default: 'Full-Stack Software Engineer'
   option :jvm, type: :boolean, default: true
   option :web, type: :boolean
   option :page_size, default: 'a4'
-  option :open, type: :boolean
+  option :open, aliases: :o, type: :boolean
   def build
-    Build.new(options[:jvm], options[:job_title], options[:page_size], options[:open]).build
+    Build.new(
+      options[:build_name],
+      options[:include_time],
+      options[:job_title],
+      options[:jvm],
+      options[:page_size],
+      options[:open]
+    ).build
   end
 end
 
 class Build
-  def initialize(is_jvm, job_title, page_size, open)
+  def initialize(build_name, include_time, job_title, is_jvm, page_size, open)
+    @build_name = build_name
+
+    dt = DateTime.now
+    date = sprintf('%d.%02d.%02d', dt.year, dt.month, dt.day)
+    time = include_time ? sprintf('-%02d.%02d', dt.hour, dt.second) : ''
+    date_time = date + time
+    if @build_name.empty?
+      @build_dir = "builds/#{date_time}"
+    else
+      @build_dir = "builds/#{date_time}-#{@build_name}"
+    end
+
     @is_jvm = is_jvm
     @job_title = job_title
     @page_size = page_size
@@ -30,21 +51,35 @@ class Build
   end
 
   def build
-    FileUtils.makedirs('build')
+    if FileTest.exist?(@build_dir)
+      FileUtils.rm_r(@build_dir)
+    end
+    FileUtils.makedirs(@build_dir)
 
     theme = ERB.new(File.read('src/cv-theme.yml.erb')).result(binding)
-    File.open('build/cv-theme.yml', 'w') do |file|
+    File.open("#{@build_dir}/cv-theme.yml", 'w') do |file|
       file.write theme
     end
 
     cv = ERB.new(File.read('src/cv.asciidoc.erb')).result(binding)
-    File.open('build/cv.asciidoc', 'w') do |file|
+    File.open("#{@build_dir}/cv.asciidoc", 'w') do |file|
       file.write cv
     end
 
-    out = @is_jvm ? 'build/cv-jvm.pdf' : 'build/cv-web.pdf'
+    out_name = if @build_name.empty?
+                @is_jvm ? 'cv-jvm.pdf' : 'cv-web.pdf'
+              else
+                "#{@build_name}.pdf"
+              end
 
-    Asciidoctor.convert_file('build/cv.asciidoc', backend: :pdf, safe: :unsafe, to_file: out)
+    out = "#{@build_dir}/#{out_name}"
+
+    Asciidoctor.convert_file(
+      "#{@build_dir}/cv.asciidoc",
+      backend: :pdf,
+      safe: :unsafe,
+      to_file: out
+    )
 
     system "open -a Preview #{out}" if @open
   end
@@ -59,5 +94,22 @@ class Build
     File.read("src/include/#{part}.asciidoc") + "\n"
   end
 end
+
+# Leave here because we may need a simple extension in the future
+# Asciidoctor::Extensions.register :unbreakable_list_items do
+#   tree_processor do
+#     process do |document|
+#       document.find_by context: :list_item do |list_item|
+#         open_block = Asciidoctor::Block.new(list_item, :open, content_model: :compound)
+#         text_literal = Asciidoctor::Inline.new(open_block, :paragraph, list_item.text, content_model: :verbatim)
+#         open_block << text_literal
+#         open_block.set_option 'unbreakable'
+#         list_item.text = nil
+#         list_item.content_model = :compound
+#         list_item.blocks << open_block
+#       end
+#     end
+#   end
+# end
 
 BuildCli.start
